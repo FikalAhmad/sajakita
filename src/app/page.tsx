69b229ebae73c8f248fae8e40621e48af2eff407
FikalAhmad <fikaalm26@gmail.com>
2024-11-10 23:08:47 +0700

    chore: minor updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import TopAd from "./components/TopAd";
 import Hero from "./components/Hero";
 import LatestNews from "./components/LatestNews";
 import PopularNews from "./components/PopularNews";
+import Sidebar from "./components/Sidebar";
+import Bisnis from "./components/Bisnis";
+import Nasional from "./components/Nasional";
+import Entertainment from "./components/Entertainment";
 
 // "use client"
 const Home = () => {
@@ -12,6 +16,14 @@ const Home = () => {
       <LatestNews />
       <PopularNews />
       <LatestNews />
+      <div className="mt-5 grid grid-cols-12 gap-5 w-full">
+        <div className="col-span-8">
+          <Entertainment />
+          <Nasional />
+          <Bisnis />
+        </div>
+        <Sidebar />
+      </div>
     </div>
   );
 };
