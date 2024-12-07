12863195cb9261ee72307c897da9aa393da67fe0
FikalAhmad <fikaalm26@gmail.com>
2024-12-08 03:04:53 +0700

    chore: minor changes

--- src/app/components/ui/ArticleCardHalf.tsx
+++ src/app/components/ui/ArticleCardHalf.tsx
@@ -29,8 +29,12 @@ const ArticleCardHalf = ({
       />
       <div className="basis-3/4 flex flex-col gap-3 w-full">
         <div className="text-[10px] sm:text-sm text-gray-600">{author}</div>
-        <h3 className="font-semibold text-[16px] line-clamp-2 sm:line-clamp-none sm:text-lg lg:text-xl">{title}</h3>
-        <p className="text-gray-600 text-sm line-clamp-2 hidden sm:block">{content}</p>
+        <h3 className="font-semibold text-[16px] line-clamp-2 sm:line-clamp-none sm:text-lg lg:text-xl">
+          {title}
+        </h3>
+        <p className="text-gray-600 text-sm line-clamp-2 hidden sm:block">
+          {content}
+        </p>
         <div className="flex flex-col sm:flex-row sm:items-center gap-3 text-[12px] sm:text-sm text-gray-600">
           {category}
           <span className="h-1 w-1 rounded-full bg-black hidden sm:block"></span>
