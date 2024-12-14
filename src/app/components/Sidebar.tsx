855116527365bfcdd37ff040109eadd56dc187b3
FikalAhmad <fikaalm26@gmail.com>
2024-12-15 03:53:00 +0700

    feat: add recommended news

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,38 +1,45 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
-import thumbnail from "@/../public/thumbnail.png";
 import SidebarAd from "./SidebarAd";
+import { usePosts } from "@/hooks/usePosts";
+import { PostsData } from "../types/PostTypes";
 
-type SidebarProps = {
-  marginTop: number;
-};
+const Sidebar = () => {
+  const { data: recommendedData, isFetching } = usePosts();
 
-const Sidebar = ({ marginTop }: SidebarProps) => {
+  if (isFetching) {
+    return <div>Loading</div>;
+  }
+
+  const recommended = recommendedData.sort(() => Math.random() - 0.5);
+
   return (
-    <div className={`hidden lg:block w-[400px] max-w-[400px] mt-${marginTop}`}>
+    <div className="hidden lg:block w-[400px] max-w-[400px]">
       <div className="sticky top-10">
         <div className="flex justify-between items-center mb-6">
           <h2 className="font-bold text-3xl">Berita Rekomendasi</h2>
         </div>
         <div className="flex flex-col gap-5">
-          {[1, 2, 3, 4, 5].map((item) => (
-            <article key={item}>
+          {recommended.slice(0, 5).map((item: PostsData) => (
+            <article key={item.id}>
               <div className="flex gap-2">
                 <Image
-                  src={thumbnail}
+                  src={process.env.NEXT_PUBLIC_API_URL + item.thumbnail.url}
                   alt="Article thumbnail"
                   width={100}
                   height={100}
-                  className="rounded-lg object-cover"
+                  className="h-[100px] w-[100px] rounded-xl object-cover"
                 />
                 <div className="flex-1 space-y-2">
-                  <Link href="/">
-                    <h3 className="font-semibold">
-                      This is example Title Page
+                  <Link href={`/${item.slug}`}>
+                    <h3 className="font-semibold line-clamp-3 text-ellipsis">
+                      {item.title}
                     </h3>
                   </Link>
                   <div className="flex items-center gap-3 text-xs text-gray-600">
-                    <span>Categories</span>
+                    <span>{item.category.name}</span>
                   </div>
                 </div>
               </div>
